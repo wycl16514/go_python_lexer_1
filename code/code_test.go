3d8deb0acd32a2a7726e37f1250e2baec91e1041
wycl16514 <yichen@real.com>
2021-12-24 19:45:10 +0800

    Add files via upload

---
+++ code/code_test.go
@@ -0,0 +1,26 @@
+package code 
+import "testing"
+
+func TestMake(t *testing.T) {
+	tests := []struct {
+		op Opcode 
+		operands []int 
+		expected []byte 
+	} {
+		{OpConstant, []int{65534}, []byte{0x0, 0xFF, 0xFE}},
+	}
+
+	for _, tt := range tests {
+		instruction := Make(tt.op, tt.operands...)
+		if len(instruction) != len(tt.expected) {
+			t.Errorf("instruction has wrong length. want=%d, got=%d",
+		len(tt.expected), len(instruction))
+		}
+
+		for i , b := range tt.expected {
+			if instruction[i] != tt.expected[i] {
+				t.Errorf("wrong byte at pos %d, want=%d, got = %d", i, b, instruction[i])
+			}
+		}
+	}
+}
